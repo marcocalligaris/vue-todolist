8d08c84e023b9ae422e70583ddeb3a47c9e79b17
marcocalligaris <marcocally@gmail.com>
2022-07-13 18:53:52 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-console.log('js ok', Vue)
+const toDoList = new Vue ({
+    el: '#root',
+    data:{
+        tasks: [
+            {
+                text: 'Fare benzina',
+                done: true,
+            },
+            {
+                text: 'Prelevare al bancomat',
+                done: false,
+            },
+            {
+                text: 'Lavare il cane',
+                done: true,
+            },
+            {
+                text: 'Lavare l\'auto',
+                done: false,
+            },
+        ]
+    }
+});
+
+
+
 
